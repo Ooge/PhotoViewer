9b3a5ab7a868cef49b72fe089db45c537635f72e
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-26 15:49:58 +0100

    Made file names random on upload

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -1,45 +1,67 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); // Stops direct access to this script.
 
+// Create our AJAX handler class, extend codeigniter's controller class.
 class Ajax extends CI_Controller {
 
     public function file_handler($type) {
+        // If a type is not set, exit with an error.
         if(!$type){
             $this->_exit(400, 'Action not specified');
         }
 
+        // Only users who are logged in can upload. Also get the user
         if (!($user = $this->m_session->get_current_user())) {
             $this->_exit(401, 'You are not authorized to access this resource');
         }
 
+        // Switch through the different types of AJAX calls, file_handler accepts
         switch($type){
             case 'upload':
-                $config['upload_path'] = FCPATH . 'uploads';
-                $config['allowed_types'] = 'gif|jpg|png';
-                $config['max_size'] = 10000;
+                // Get the file extension through exploding the file name by . and
+                // getting the last element.
+                $fileExtension = end(explode('.',$_FILES['userfile']['name']));
 
+                // Here we setup our upload configuration
+                $config['upload_path'] = FCPATH . 'uploads';    // Where we want to upload
+                $config['allowed_types'] = 'gif|jpg|png';       // Filetypes we allow
+                $config['max_size'] = 10000;                    // Allow up to 10MB files
+                // Using a helper function, generate a random name for the file
+                $config['file_name'] =  generate_random(10) . $fileExtension;
+
+                // Load the upload class, parsing in the config values set above.
                 $this->load->library('upload', $config);
-
+                // Do our upload, encapsulate it in an if statement to see if it
+                // was successful or not.
                 if(!$this->upload->do_upload('userfile')) {
                     // Error occurred while uploading
                     $this->_exit(400,'Error uploading File', array('error' => $this->upload->display_errors()));
                 } else {
+                    // Was successful, exit the user, parsing the uploaded files info.
                     $this->_exit(200, null, $this->upload->data());
                 }
                 break;
         }
     }
 
+
+    // Our custom exit() function
     function _exit($code, $reason = null, $add = array()) {
+        // Set our headers correctly to the datatype we are sending
         header('Content-Type: application/json');
+        // Send our response code
         header('X-PHP-Response-Code: ' . $code, true, $code);
+        // If we have a reason, parse an error reason
         if ($reason) {
             header('X-OG-Error: ' . $reason);
         }
+        // Set success to true of false, depending on if the code == 200
         $array = array('success' => ($code == 200));
+        // if its not 200 then set the code and reason in our error report
         if ($code != 200) {
             $array['error_code'] = $code;
             $array['error_string'] = $reason;
         }
+        // Exit with the data set.
         exit(json_encode(array_merge($array, $add)));
     }
 }
