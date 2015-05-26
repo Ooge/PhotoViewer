71a34571bc809737e935f2dc59c644582a651657
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-26 12:38:25 +0100

    Login Styling

--- application/pages/login/register.php
+++ application/pages/login/register.php
@@ -9,16 +9,32 @@ ob_start();
 
     <span style="color:red; font-weight:bold;"><?php echo validation_errors(); ?></span>
     <?php echo form_open('register_user', array('data-abide' => 1)); ?>
-    <input type="text" name="first_name" value="<?php echo set_value('first_name'); ?>" id="first_name" placeholder="First Name">
-    <input type="text" name="last_name" value="<?php echo set_value('last_name'); ?>" id="last_name" placeholder="Last Name">
-    <input type="text" name="username" value="<?php echo set_value('username'); ?>" id="username" placeholder="Username">
-    <input type="email" name="email" value="<?php echo set_value('email'); ?>" id="email" placeholder="E-Mail">
-    <?php echo form_dropdown('day', build_days(), set_value('day', 'err'), 'id="day_dropdown"'); ?>
-    <?php echo form_dropdown('month', build_months(), set_value('month', 'err'), 'style="width:100px" id="month_dropdown"'); ?>
-    <?php echo form_dropdown('year', build_years(), set_value('year', 'err'), 'id="year_dropdown"'); ?>
-    <input type="password" name="password" value="" id="password" placeholder="Password">
-    <input type="password" name="password_repeat" value="" id="password_repeat" placeholder="Confirm Password">
-    <input type="submit" name="register_submit" value="Create User" class="button success">
+    <div class="input-group">
+        <input type="text" name="first_name" value="<?php echo set_value('first_name'); ?>" id="first_name" placeholder="First Name">
+    </div>
+    <div class="input-group">
+        <input type="text" name="last_name" value="<?php echo set_value('last_name'); ?>" id="last_name" placeholder="Last Name">
+    </div>
+    <div class="input-group">
+        <input type="text" name="username" value="<?php echo set_value('username'); ?>" id="username" placeholder="Username">
+    </div>
+    <div class="input-group">
+        <input type="email" name="email" value="<?php echo set_value('email'); ?>" id="email" placeholder="E-Mail">
+    </div>
+    <div class="input-group">
+        <?php echo form_dropdown('day', build_days(), set_value('day', 'err'), 'id="day_dropdown"'); ?>
+        <?php echo form_dropdown('month', build_months(), set_value('month', 'err'), 'style="width:100px" id="month_dropdown"'); ?>
+        <?php echo form_dropdown('year', build_years(), set_value('year', 'err'), 'id="year_dropdown"'); ?>
+    </div>
+    <div class="input-group">
+        <input type="password" name="password" value="" id="password" placeholder="Password">
+    </div>
+    <div class="input-group">
+        <input type="password" name="password_repeat" value="" id="password_repeat" placeholder="Confirm Password">
+    </div>
+    <div class="input-group">
+        <input type="submit" name="register_submit" value="Create User" class="button success">
+    </div>
     <?php echo form_close(); ?>
     <hr>
     <p><a href="/login">Back to Login</a></p>
