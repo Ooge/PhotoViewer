fd6c22a8ebb8f13e9edd2dcade2be77cad0aff0b
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-26 11:36:00 +0100

    Login Styling

--- application/pages/login/login.php
+++ application/pages/login/login.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$stylesheets = array('assets/css/login/login.css');
+$stylesheets = array('assets/css/login.css');
 $scripts = array();
 $title = 'Login';
 ob_start();
