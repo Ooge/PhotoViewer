d42e977a730879833e4d1f78b313d35ef9e74aee
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-27 11:04:52 +0100

    Thats what happens when you reuse old code ryan

--- application/classes/Image.php
+++ application/classes/Image.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class User extends TableObject {
+class Image extends TableObject {
     public $id;
     private $db;
 
