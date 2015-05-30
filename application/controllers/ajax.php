b0e2a213dcce555f5bda92a875f26fe09c435cea
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-30 14:20:14 +0100

    AJAX: browser data

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -76,7 +76,7 @@ class Ajax extends CI_Controller {
                 $query = $this->db->query($sql);
                 if($query->num_rows() > 0){
                     $results = $query->result_array();
-                    $this->_exit(200, null, $results);
+                    $this->_exit(200, null, array('data' => $results));
                 }
                 break;
         }
