e8ef1b019a4c560e1f11ae773b0d3fdd549f1440
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-30 13:56:49 +0100

    Testing Pie Charts

--- assets/js/vendor/highcharts.theme.dark.js
+++ assets/js/vendor/highcharts.theme.dark.js
@@ -14,9 +14,7 @@ Highcharts.theme = {
    colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
       "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
    chart: {
-      backgroundColor: {
-         
-      },
+      backgroundColor: '#1a1a1a',
       style: {
          fontFamily: "'Unica One', sans-serif"
       }
