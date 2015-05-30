cd9f98f5c28d638c72cec327687d68f92d32812b
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-30 13:16:16 +0100

    Admin panel styling

--- application/pages/admin.php
+++ application/pages/admin.php
@@ -9,8 +9,13 @@ $user = $CI->m_session->get_current_user();
 ob_start();
 ?>
 <h1>Admin Panel</h1>
-<h3>Welcome, <?php echo $user->username; ?> to the admin panel for i.ooge.uk.</h3>
-
+<h3>Hi <?php echo $user->username; ?>.</h3>
+<div class="admin-section">
+stats
+</div>
+<div class="admin-section">
+morestats
+</div>
 <?php
 $content = ob_get_contents();
 ob_end_clean();
