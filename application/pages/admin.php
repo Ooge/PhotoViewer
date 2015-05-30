fc4ba9e32d28ba13464640b96631b4b6f6e65091
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-30 13:33:04 +0100

    Testing Pie Charts

--- application/pages/admin.php
+++ application/pages/admin.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $stylesheets = array(base_url('assets/css/admin.css'));
-$scripts = array(base_url('assets/js/admin.js'));
+$scripts = array(base_url('assets/js/vendor/highcharts.js'), base_url('assets/js/admin.js'));
 $title = 'Admin';
 $CI =& get_instance();
 
@@ -9,7 +9,7 @@ $user = $CI->m_session->get_current_user();
 ob_start();
 ?>
 <h1>Hi <?php echo $user->username; ?> - Welcome to the Admin Panel</h1>
-<div class="admin-section">
+<div class="admin-section" id="browser_chart">
 stats
 </div>
 <div class="admin-section">
---
+++ assets/js/admin.js
@@ -0,0 +1,49 @@
+Ooge.admin = {
+    handlers: {},
+    init: function() {
+        $('#browser_chart').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: null,
+            },
+            title: {
+                text: 'Common Browser Types'
+            },
+            tooltop: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Common Browsers',
+                data: [
+                    ['Firefox',   45.0],
+                    ['IE',       26.8],
+                    {
+                        name: 'Chrome',
+                        y: 12.8,
+                        sliced: true,
+                        selected: true
+                    },
+                    ['Safari',    8.5],
+                    ['Opera',     6.2],
+                    ['Others',   0.7]
+                ]
+            }]
+        });
+    }
+};
+Ooge.admin.init();
