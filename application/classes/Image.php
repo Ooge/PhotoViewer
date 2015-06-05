96e27a1e39040e77d54e85532f55d5096352ce24
Ryan Thorn <ryan@ryanthorn.uk>
2015-06-05 19:28:35 +0100

    Testing Thumbnails: debugging

--- application/classes/Image.php
+++ application/classes/Image.php
@@ -33,7 +33,7 @@ class Image extends TableObject {
 
 
     public function get_thumbnail() {
-        /*$imagick = new Imagick(realpath(FCPATH . $this->file));
+        $imagick = new Imagick(realpath(FCPATH . $this->file));
         $imagick->thumbnailImage(219, 219);
 
         $ext = explode('.', $this->file);
@@ -49,8 +49,7 @@ class Image extends TableObject {
                 break;
         }
 
-        return $imagick->getImageBlob();*/
-        return $this->file;
+        return $imagick->getImageBlob();
     }
 
     /* */ /* */ /* */ /* */ /* */
--- application/pages/home.php
+++ application/pages/home.php
@@ -13,7 +13,8 @@ ob_start();
 	<?php
 		if($latestImages != 0){
 			foreach($latestImages as $image){
-				echo '<a href="'.base_url($image->gid).'"><div class="gallery-item"><img src="'.$image->get_thumbnail().'" alt="'.$image->title.'" title="'.$image->title.'" /></div></a>';
+				//echo '<a href="'.base_url($image->gid).'"><div class="gallery-item"><img src="'.$image->get_thumbnail().'" alt="'.$image->title.'" title="'.$image->title.'" /></div></a>';
+				var_dump($image->get_thumbnail());
 			}
 		} else {
 			echo 'No images posted. <a class="upload_toggle" href="javascript:void(0);">Be the first</a>';
